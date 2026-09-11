e99f22df18a2276a4a5b9281cc6b5161eb35546c
Hari4710 <o.harikrishna471@gmail.com>
2026-09-11 07:14:41 +0530

    Add login page component

---
+++ web/app/login/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+export default function Login(){
+ return(
+  <div style={{padding:20}}>
+   <h2>Nexlance Login - Sec 6</h2>
+   <p>No Google Login</p>
+   <input placeholder="User ID"/><br/><br/>
+   <input type="password" placeholder="Password"/><br/><br/>
+   <input placeholder="2FA"/><br/><br/>
+   <button>Login</button>
+  </div>
+ )
+}
