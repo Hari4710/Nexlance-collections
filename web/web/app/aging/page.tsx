074128f44e8df567e16f576067d5d259c7c96c48
Hari4710 <o.harikrishna471@gmail.com>
2026-09-11 07:52:53 +0530

    Create page.tsx

---
+++ web/web/app/aging/page.tsx
@@ -0,0 +1,4 @@
+"use client"
+export default function Aging(){
+ return <div style={{padding:20}}><h1>Aging Report</h1><p>0-30 days | 31-60 | 61-90 | 90+ days bucket</p><p>Overdue amount show avutundi</p></div>
+}
