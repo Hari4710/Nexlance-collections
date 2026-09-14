cf92fb44ad624e3d06f1191228960254dd79ead5
Hari4710 <o.harikrishna471@gmail.com>
2026-09-14 11:24:08 +0530

    Delete web/web/app/aging/page.tsx

--- web/web/app/aging/page.tsx
+++
@@ -1,4 +0,0 @@
-"use client"
-export default function Aging(){
- return <div style={{padding:20}}><h1>Aging Report</h1><p>0-30 days | 31-60 | 61-90 | 90+ days bucket</p><p>Overdue amount show avutundi</p></div>
-}
